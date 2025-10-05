00e5192e7f8b925a11e581c12a2fc96ca21f26ed
23tareyg <23tareyg@gmail.com>
2025-10-05 01:12:38 -0400

    Prints proper forecasting data to console

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,44 @@ using json = nlohmann::json;
 int main() {
     std::ifstream f("../data/test.json");
     json data = json::parse(f);
-
     std::vector<Stock> * stocks = new std::vector<Stock>;
+    std::mt19937 prng;
+
+    double timestep = data["timestep_minutes"];
+    double duration = data["duration_minutes"];
+    double min_per_year = 252*6.5*60;
+    double dt = timestep / min_per_year;
 
     for (const auto& stock : data["stocks"]) {
         Stock s(stock);
         stocks->push_back(s);
     }
 
-    std::cout << stocks->at(0).name;
+    std::vector<std::vector<double>> res;
+    int num_steps = duration / timestep;
+    res.resize(stocks->size(), std::vector<double>(num_steps));
+
+    for (int i = 0; i < (*stocks).size(); i++) {
+        int temp = duration;
+        GBMModel model;
+        int step = 0;
+        while (temp > 0) {
+            double calc = model.step(stocks->at(i).price, stocks->at(i).mu, stocks->at(i).sigma, dt, prng);
+            res[i][step] = calc;
+            stocks->at(i).price = calc;
+            step++;
+            temp -= timestep;
+        }
+    }
+
+    for(auto i : res) {
+        std::cout << "Stock 1:\n";
+        for (auto j : i) {
+            std::cout << j << ',' << '\n';
+        }
+    }
+
+    delete stocks;
+
+    return 0;
 }

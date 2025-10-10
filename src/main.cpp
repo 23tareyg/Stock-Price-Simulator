4c728605727b19e892ce0742b08e12680df4e83f
23tareyg <23tareyg@gmail.com>
2025-10-10 00:29:55 -0400

    SFML set up

--- src/main.cpp
+++ src/main.cpp
@@ -28,38 +28,18 @@ int main() {
     ////////////
 
     // Create the main window
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML window");
- 
-    // Load a sprite to display
-    const sf::Texture texture("cute_image.jpg");
-    sf::Sprite sprite(texture);
- 
-    // Create a graphical text to display
-    const sf::Font font("arial.ttf");
-    sf::Text text(font, "Hello SFML", 50);
- 
- 
-    // Start the game loop
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "My Window");
+
     while (window.isOpen())
     {
-        // Process events
-        while (const std::optional event = window.pollEvent())
-        {
-            // Close window: exit
-            if (event->is<sf::Event::Closed>())
+        
+        while (std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
         }
- 
-        // Clear screen
-        window.clear();
- 
-        // Draw the sprite
-        window.draw(sprite);
- 
-        // Draw the string
-        window.draw(text);
- 
-        // Update the window
+
+        window.clear(sf::Color::Black);
         window.display();
     }
 
